4395e05206fd274fead1a9240d3692448a51ce69
vaibhav-xi <vaibhav28890@gmail.com>
2026-07-04 21:43:33 +0530

    Debug Mesh

--- render_es2/mesh.py
+++ render_es2/mesh.py
@@ -1,17 +1,31 @@
 import ctypes
-from array import array
+
+import numpy as np
 
 from OpenGL.GL import *
-import numpy as np
+
 from render_es2.vao import VAO
+
 
 class Mesh:
 
     def __init__(self):
-        
+
         self.vao = VAO()
+
         self.vbo = glGenBuffers(1)
+
         self.count = 0
+
+    # ---------------------------------------------------------
+
+    def _check(self, where):
+
+        err = glGetError()
+
+        if err != GL_NO_ERROR:
+
+            print(f"[GL ERROR] {where}: {hex(err)}")
 
     # ---------------------------------------------------------
 
@@ -24,16 +38,36 @@ class Mesh:
 
         self.count = len(vertices) // 5
 
-        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
-
-        glBufferData(
-            GL_ARRAY_BUFFER,
-            vertices.nbytes,
-            vertices,
-            GL_DYNAMIC_DRAW,
+        print(
+            "Uploading",
+            self.count,
+            "vertices",
         )
 
-        glBindBuffer(GL_ARRAY_BUFFER, 0)
+        glBindBuffer(
+            GL_ARRAY_BUFFER,
+            self.vbo,
+        )
+
+        self._check("BindBuffer(update)")
+
+        glBufferData(
+
+            GL_ARRAY_BUFFER,
+
+            vertices.nbytes,
+
+            vertices,
+
+            GL_DYNAMIC_DRAW,
+
+        )
+
+        self._check("BufferData")
+
+        #
+        # Intentionally leave buffer bound.
+        #
 
     # ---------------------------------------------------------
 
@@ -41,10 +75,17 @@ class Mesh:
 
         if self.count == 0:
             return
-        
+
         self.vao.bind()
 
-        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        self._check("VAO bind")
+
+        glBindBuffer(
+            GL_ARRAY_BUFFER,
+            self.vbo,
+        )
+
+        self._check("BindBuffer(draw)")
 
         #
         # Position
@@ -52,14 +93,25 @@ class Mesh:
 
         glEnableVertexAttribArray(0)
 
+        self._check("EnableAttrib0")
+
         glVertexAttribPointer(
+
             0,
+
             2,
+
             GL_FLOAT,
+
             GL_FALSE,
+
             5 * 4,
+
             ctypes.c_void_p(0),
+
         )
+
+        self._check("AttribPointer0")
 
         #
         # Color
@@ -67,22 +119,44 @@ class Mesh:
 
         glEnableVertexAttribArray(1)
 
+        self._check("EnableAttrib1")
+
         glVertexAttribPointer(
+
             1,
+
             3,
+
             GL_FLOAT,
+
             GL_FALSE,
+
             5 * 4,
+
             ctypes.c_void_p(2 * 4),
+
+        )
+
+        self._check("AttribPointer1")
+
+        print(
+            "Drawing",
+            self.count,
+            "vertices",
         )
 
         glDrawArrays(
+
             GL_LINES,
+
             0,
+
             self.count,
+
         )
 
-        # glDisableVertexAttribArray(0)
-        # glDisableVertexAttribArray(1)
+        self._check("DrawArrays")
 
-        glBindBuffer(GL_ARRAY_BUFFER, 0)
+        #
+        # Leave attributes enabled for now.
+        #
