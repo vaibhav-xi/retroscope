c0b855cf205284d965e1cc8725ea1e76c47a2c3a
vaibhav-xi <vaibhav28890@gmail.com>
2026-07-04 21:23:53 +0530

    geometry RGB helper

--- render_es2/geometry.py
+++ render_es2/geometry.py
@@ -11,6 +11,15 @@ from render.primitives import Polyline
 
 
 class Geometry:
+    
+    @staticmethod
+    def rgb(color):
+
+        return (
+            color[0] / 255.0,
+            color[1] / 255.0,
+            color[2] / 255.0,
+        )
 
     @staticmethod
     def build(frame):
@@ -39,12 +48,22 @@ class Geometry:
                 x1, y1 = points[i]
                 x2, y2 = points[i + 1]
 
+                r, g, b = Geometry.rgb(primitive.color)
+
                 vertices.extend([
+
                     Geometry._x(x1),
                     Geometry._y(y1),
+                    r,
+                    g,
+                    b,
 
                     Geometry._x(x2),
                     Geometry._y(y2),
+                    r,
+                    g,
+                    b,
+
                 ])
 
         return vertices
