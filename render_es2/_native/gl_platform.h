a8505baf21117f19a234ea5eb783288c08b0213b
vaibhav-xi <vaibhav28890@gmail.com>
2026-07-10 14:58:46 +0530

    new platform header

--- render_es2/_native/gl_platform.h
+++ render_es2/_native/gl_platform.h
@@ -7,13 +7,44 @@
 #elif defined(__arm__) || defined(__aarch64__)
 
 #include <GLES2/gl2.h>
-
-#define GL_GLEXT_PROTOTYPES
 #include <GLES2/gl2ext.h>
 
-#define glGenVertexArrays    glGenVertexArraysOES
-#define glBindVertexArray    glBindVertexArrayOES
-#define glDeleteVertexArrays glDeleteVertexArraysOES
+/*
+ * Raspberry Pi Mesa exports the core VAO symbols:
+ *
+ *     glGenVertexArrays
+ *     glBindVertexArray
+ *     glDeleteVertexArrays
+ *
+ * but GLES2 headers only declare the OES versions.
+ *
+ * Declare the core entry points ourselves.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+GL_APICALL void GL_APIENTRY
+glGenVertexArrays(
+    GLsizei n,
+    GLuint *arrays
+);
+
+GL_APICALL void GL_APIENTRY
+glBindVertexArray(
+    GLuint array
+);
+
+GL_APICALL void GL_APIENTRY
+glDeleteVertexArrays(
+    GLsizei n,
+    const GLuint *arrays
+);
+
+#ifdef __cplusplus
+}
+#endif
 
 #else
 
