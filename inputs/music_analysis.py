236b9adf4bb3e3a15a2d60077b2e86685424fd84
vaibhav-xi <vaibhav28890@gmail.com>
2026-07-16 14:41:56 +0530

    latency 0.2

--- inputs/music_analysis.py
+++ inputs/music_analysis.py
@@ -130,7 +130,7 @@ class MusicAnalyzer(AudioInput):
         input_gain: float = 1.0,
         channel: int = 0,
         channels: int = 1,
-        latency=None,
+        latency=0.2,
         muted: bool = False,
         stereo: bool = False,
         enable_band_waveforms: bool = True,
